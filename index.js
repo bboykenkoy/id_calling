abd9a68e74fe34a35b8c127f06cee35fe6270ac3
Mr Phuong <phuonghv20193@gmail.com>
2017-11-06 15:52:47 +0700

    123

--- index.js
+++ index.js
@@ -201,18 +201,19 @@ io.on('connection', function(socket) { // Incoming connections from clients
                                 if (data.length > 0) {
                                     socket.broadcast.emit('calling', { key: user.key, friend_key: data[0].key, result: 1, type: "result"});
                                     console.log("Emit: " + user.key + " to " + data[0].key);
-                                    socket.broadcast.emit('K_Signal_Call', {message:"user not found",result: 0, type: "result"});
+                                    socket.broadcast.emit('K_Signal_Call', [{message:"user not found",result: 0, type: "result"}]);
                                 } else {
-                                    socket.broadcast.emit('calling', { message:"user not found", result: 0, type: "result"});
-                                    socket.broadcast.emit('K_Signal_Call', {message:"user not found",result: 0, type: "result"});
+                                    socket.broadcast.emit('calling',[{ message:"user not found", result: 0, type: "result"}]);
+                                    socket.broadcast.emit('K_Signal_Call', p{message:"user not found",result: 0, type: "result"}]);
                                     console.log("user not found");
                                 }
                             }
                         });
                     }else{
                         console.log("user not found");
-                        socket.broadcast.emit('calling', {message:"user not found",result: 0, type: "result"});
-                        socket.broadcast.emit('K_Signal_Call', {message:"user not found",result: 0, type: "result"});
+                        socket.broadcast.emit("K_Signal_Call", msg);
+                        socket.broadcast.emit('calling', [{message:"user not found",result: 0, type: "result"}]);
+                        socket.broadcast.emit('K_Signal_Call', [{message:"user not found",result: 0, type: "result"}]);
                         
                     }
                 }
