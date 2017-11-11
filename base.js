d8837975d9819cf8f6352385fa39284a3dcf746c
Dat <dat12a21996@gmail.com>
2017-11-11 09:22:19 +0700

    213
    321

--- base.js
+++ base.js
@@ -74,6 +74,7 @@ function isJsonString(str) {
     try {
         JSON.parse(str);
     } catch (e) {
+        console.log(e);
         return false;
     }
     return true;
@@ -83,6 +84,7 @@ function isDecrypt(str) {
     try {
         decrypt(str);
     } catch (e) {
+        console.log(e);
         return false;
     }
     return true;
