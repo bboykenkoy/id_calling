70e6d08ce42f0f09efc4cf1617d0b7d642e0939c
Dat <dat12a21996@gmail.com>
2017-11-11 11:11:39 +0700

    qwe
    ewq

--- base.js
+++ base.js
@@ -3,9 +3,9 @@ var md5 = require('md5');
 var mysql = require('mysql');
 var escapeSQL = require('sqlstring');
 var config = require('./config.js');
-// var crypto = require('crypto'),
-//     algorithm = 'aes-256-ctr',
-//     password = 'd6F3Efeq';
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
 // Gửi email
 var nodemailer = require('nodemailer');
 var urlParser = bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 });
@@ -58,58 +58,24 @@ client.query("SET CHARACTER SET utf8mb4", function(error, results, fields) {
 });
 // Kết thúc phần khởi tạo kết nối
 
-// function encrypt(text) {
-//     var cipher = crypto.createCipher(algorithm, password);
-//     var crypted = cipher.update(text, 'utf8', 'hex');
-//     crypted += cipher.final('hex');
-//     return crypted;
-// }
-
-// function decrypt(text) {
-//     var decipher = crypto.createDecipher(algorithm, password);
-//     var dec = decipher.update(text, 'hex', 'utf8');
-//     dec += decipher.final('utf8');
-//     return dec;
-// }
-
-
-
-var crypto = require('crypto'),
-    algorithm = 'aes-256-gcm',
-    password = '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
-    // do not use a global iv for production, 
-    // generate a new one for each encryption
-    iv = '60iP0h6vJoEa'
-
 function encrypt(text) {
-    var cipher = crypto.createCipheriv(algorithm, password, iv)
-    var encrypted = cipher.update(text, 'utf8', 'hex')
-    encrypted += cipher.final('hex');
-    var tag = cipher.getAuthTag();
-    return {
-        content: encrypted,
-        tag: tag
-    };
+    var cipher = crypto.createCipher(algorithm, password);
+    var crypted = cipher.update(text, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+    return crypted;
 }
 
-function decrypt(encrypted) {
-    var decipher = crypto.createDecipheriv(algorithm, password, iv)
-    decipher.setAuthTag(encrypted.tag);
-    var dec = decipher.update(encrypted.content, 'hex', 'utf8')
+function decrypt(text) {
+    var decipher = crypto.createDecipher(algorithm, password);
+    var dec = decipher.update(text, 'hex', 'utf8');
     dec += decipher.final('utf8');
     return dec;
 }
-
-
-
-
-
 
 function isJsonString(str) {
     try {
         JSON.parse(str);
     } catch (e) {
-        console.log(e);
         return false;
     }
     return true;
@@ -119,7 +85,6 @@ function isDecrypt(str) {
     try {
         decrypt(str);
     } catch (e) {
-        console.log(e);
         return false;
     }
     return true;
