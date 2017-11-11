4c88621e8cc481f5c844b25b9680914af4c36f59
Dat <dat12a21996@gmail.com>
2017-11-11 09:20:59 +0700

    213
    123

--- base.js
+++ base.js
@@ -195,6 +195,7 @@ module.exports = class Authenticate {
                                 } else {
                                     callback(false);
                                     console.log("ACCESS_TOKEN: 1.11");
+                                    client.query("DELETE FROM `tokens` WHERE `access_token`='" + access_token + "' AND `users_key`='" + key + "'");
                                 }
                             } else {
                                 callback(false);
@@ -202,152 +203,147 @@ module.exports = class Authenticate {
                             }
                         }
                     });
-                } else {
+                } catch (e) {
+                    console.log("ACCESS_TOKEN: 1.44");
                     callback(false);
-                    console.log("ACCESS_TOKEN: 1.33");
-                    client.query("DELETE FROM `tokens` WHERE `access_token`='" + access_token + "' AND `users_key`='" + key + "'");
                 }
-            } catch (e) {
-                console.log("ACCESS_TOKEN: 1.44");
+            } else {
+                console.log("ACCESS_TOKEN: 1.55");
                 callback(false);
             }
         } else {
-            console.log("ACCESS_TOKEN: 1.55");
+            console.log("ACCESS_TOKEN: 2.0");
             callback(false);
         }
-    } else {
-        console.log("ACCESS_TOKEN: 2.0");
-        callback(false);
     }
-}
 
-getFriendByKey(key, callback) {
-    var sqlSelect = "SELECT `key`, `email`, `username`, `nickname`, `created_at`, `avatar`, `cover`, `sex`, `birthday`, `last_active`, `latitude`, `longitude`, `status`, `facebook_point`, `country`, `city`, `img_width`, `img_height`, `is_active`";
-    var sqlWhere = " FROM `users` WHERE `key`='" + key + "'";
-    client.query(sqlSelect + sqlWhere, function(error, data, fields) {
-        if (error) {
-            console.log(error);
-            callback();
-        } else {
-            if (data.length > 0) {
-                callback(data[0]);
-            } else {
+    getFriendByKey(key, callback) {
+        var sqlSelect = "SELECT `key`, `email`, `username`, `nickname`, `created_at`, `avatar`, `cover`, `sex`, `birthday`, `last_active`, `latitude`, `longitude`, `status`, `facebook_point`, `country`, `city`, `img_width`, `img_height`, `is_active`";
+        var sqlWhere = " FROM `users` WHERE `key`='" + key + "'";
+        client.query(sqlSelect + sqlWhere, function(error, data, fields) {
+            if (error) {
+                console.log(error);
                 callback();
-            }
-        }
-    });
-}
-baseSelectFriendSQL() {
-    return "`key`, `email`, `username`, `nickname`, `created_at`, `avatar`, `cover`, `sex`, `birthday`, `last_active`, `latitude`, `longitude`, `status`, `facebook_point`, `country`, `city`, `img_width`, `img_height`, `is_active`";
-}
-getFriendBySQL(sqlWhere, callback) {
-    var sqlSelect = "SELECT `key`, `email`, `username`, `nickname`, `created_at`, `avatar`, `cover`, `sex`, `birthday`, `last_active`, `latitude`, `longitude`, `status`, `facebook_point`, `country`, `city`, `img_width`, `img_height`, `is_active` FROM `users` ";
-    client.query(sqlSelect + sqlWhere, function(error, data, fields) {
-        if (error) {
-            console.log(error);
-            callback();
-        } else {
-            if (data.length > 0) {
-                callback(data[0]);
             } else {
+                if (data.length > 0) {
+                    callback(data[0]);
+                } else {
+                    callback();
+                }
+            }
+        });
+    }
+    baseSelectFriendSQL() {
+        return "`key`, `email`, `username`, `nickname`, `created_at`, `avatar`, `cover`, `sex`, `birthday`, `last_active`, `latitude`, `longitude`, `status`, `facebook_point`, `country`, `city`, `img_width`, `img_height`, `is_active`";
+    }
+    getFriendBySQL(sqlWhere, callback) {
+        var sqlSelect = "SELECT `key`, `email`, `username`, `nickname`, `created_at`, `avatar`, `cover`, `sex`, `birthday`, `last_active`, `latitude`, `longitude`, `status`, `facebook_point`, `country`, `city`, `img_width`, `img_height`, `is_active` FROM `users` ";
+        client.query(sqlSelect + sqlWhere, function(error, data, fields) {
+            if (error) {
+                console.log(error);
                 callback();
-            }
-        }
-    });
-}
-getMeByKey(key, callback) {
-    var sqlSelect = "SELECT * FROM `users`";
-    var sqlWhere = " WHERE `key`='" + key + "'";
-    client.query(sqlSelect + sqlWhere, function(error, data, fields) {
-        if (error) {
-            console.log(error);
-            callback();
-        } else {
-            if (data.length > 0) {
-                callback(data[0]);
             } else {
+                if (data.length > 0) {
+                    callback(data[0]);
+                } else {
+                    callback();
+                }
+            }
+        });
+    }
+    getMeByKey(key, callback) {
+        var sqlSelect = "SELECT * FROM `users`";
+        var sqlWhere = " WHERE `key`='" + key + "'";
+        client.query(sqlSelect + sqlWhere, function(error, data, fields) {
+            if (error) {
+                console.log(error);
                 callback();
-            }
-        }
-    });
-}
-isFollowing(key, friend_key, callback) {
-    client.query("SELECT * FROM `contacts` WHERE `users_key`='" + key + "' AND `friend_key`='" + friend_key + "' AND `is_following`=1", function(e, d, f) {
-        if (e) {
-            console.log(e);
-            callback(0);
-        } else {
-            if (d.length > 0) {
-                callback(1);
             } else {
+                if (data.length > 0) {
+                    callback(data[0]);
+                } else {
+                    callback();
+                }
+            }
+        });
+    }
+    isFollowing(key, friend_key, callback) {
+        client.query("SELECT * FROM `contacts` WHERE `users_key`='" + key + "' AND `friend_key`='" + friend_key + "' AND `is_following`=1", function(e, d, f) {
+            if (e) {
+                console.log(e);
                 callback(0);
-            }
-        }
-    });
-}
-getRelationship(users_key, friend_key, ketqua) {
-    var userSQL = "SELECT * FROM `blocks` WHERE `friend_key`='" + friend_key + "' AND `users_key`='" + users_key + "'";
-    client.query(userSQL, function(eBlock, dBlock, fBlock) {
-        if (eBlock) {
-            console.log(eBlock);
-            ketqua(5);
-        } else {
-            if (dBlock.length > 0) {
-                ketqua(0);
             } else {
-                var userSQL = "SELECT * FROM `blocks` WHERE `friend_key`='" + users_key + "' AND `users_key`='" + friend_key + "'";
-                client.query(userSQL, function(eBlock, dBlock, fBlock) {
-                    if (eBlock) {
-                        console.log(eBlock);
-                        ketqua(5);
-                    } else {
-                        if (dBlock.length > 0) {
-                            ketqua(1);
-                        } else {
-                            var userSQL = "SELECT * FROM `requests` WHERE `friend_key`='" + users_key + "' AND `users_key`='" + friend_key + "'";
-                            client.query(userSQL, function(error, data, fields) {
-                                if (error) {
-                                    console.log(error);
-                                    ketqua(5);
-                                } else {
-                                    if (data.length > 0) {
-                                        ketqua(2);
-                                    } else {
-                                        //---
-                                        var userSQL2 = "SELECT * FROM `requests` WHERE `friend_key`='" + friend_key + "' AND `users_key`='" + users_key + "'";
-                                        client.query(userSQL2, function(error1, data1, fields1) {
-                                            if (error1) {
-                                                console.log(error1);
-                                                ketqua(5);
-                                            } else {
-                                                if (data1.length > 0) {
-                                                    ketqua(3);
-                                                } else {
-                                                    //---
-                                                    var userSQL2 = "SELECT * FROM `contacts` WHERE `friend_key`='" + friend_key + "' AND `users_key`='" + users_key + "'";
-                                                    client.query(userSQL2, function(error2, data2, fields2) {
-                                                        if (error2) {
-                                                            console.log(error2);
-                                                            ketqua(5);
-                                                        } else {
-                                                            if (data2.length > 0) {
-                                                                ketqua(4);
-                                                            } else {
-                                                                ketqua(5);
-                                                            }
-                                                        }
-                                                    });
-                                                }
-                                            }
-                                        });
-                                    }
-                                }
-                            });
-                        }
-                    }
-                });
+                if (d.length > 0) {
+                    callback(1);
+                } else {
+                    callback(0);
+                }
             }
-        }
-    });
-}
+        });
+    }
+    getRelationship(users_key, friend_key, ketqua) {
+        var userSQL = "SELECT * FROM `blocks` WHERE `friend_key`='" + friend_key + "' AND `users_key`='" + users_key + "'";
+        client.query(userSQL, function(eBlock, dBlock, fBlock) {
+            if (eBlock) {
+                console.log(eBlock);
+                ketqua(5);
+            } else {
+                if (dBlock.length > 0) {
+                    ketqua(0);
+                } else {
+                    var userSQL = "SELECT * FROM `blocks` WHERE `friend_key`='" + users_key + "' AND `users_key`='" + friend_key + "'";
+                    client.query(userSQL, function(eBlock, dBlock, fBlock) {
+                        if (eBlock) {
+                            console.log(eBlock);
+                            ketqua(5);
+                        } else {
+                            if (dBlock.length > 0) {
+                                ketqua(1);
+                            } else {
+                                var userSQL = "SELECT * FROM `requests` WHERE `friend_key`='" + users_key + "' AND `users_key`='" + friend_key + "'";
+                                client.query(userSQL, function(error, data, fields) {
+                                    if (error) {
+                                        console.log(error);
+                                        ketqua(5);
+                                    } else {
+                                        if (data.length > 0) {
+                                            ketqua(2);
+                                        } else {
+                                            //---
+                                            var userSQL2 = "SELECT * FROM `requests` WHERE `friend_key`='" + friend_key + "' AND `users_key`='" + users_key + "'";
+                                            client.query(userSQL2, function(error1, data1, fields1) {
+                                                if (error1) {
+                                                    console.log(error1);
+                                                    ketqua(5);
+                                                } else {
+                                                    if (data1.length > 0) {
+                                                        ketqua(3);
+                                                    } else {
+                                                        //---
+                                                        var userSQL2 = "SELECT * FROM `contacts` WHERE `friend_key`='" + friend_key + "' AND `users_key`='" + users_key + "'";
+                                                        client.query(userSQL2, function(error2, data2, fields2) {
+                                                            if (error2) {
+                                                                console.log(error2);
+                                                                ketqua(5);
+                                                            } else {
+                                                                if (data2.length > 0) {
+                                                                    ketqua(4);
+                                                                } else {
+                                                                    ketqua(5);
+                                                                }
+                                                            }
+                                                        });
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
 }
